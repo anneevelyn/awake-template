ea245ea2ade24dccbf928ef953cc956321135852
anneevelyn <62508929+anneevelyn@users.noreply.github.com>
2020-04-12 13:02:24 -0700

    new quote card instead of image

---
+++ components/cards/QuoteCardTwo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card quote-card-two">
+    <div class="card-content">
+      <p class="title">
+        “Listening is not the same as hearing and hearing is not the same as listening”
+      </p>
+      <p class="subtitle">
+        ~ Pauline Oliveros
+      </p>
+    </div>
+    <footer class="card-footer">
+      <div class="card-footer-item">
+        <span> Share on <a href="#">Twitter</a> </span>
+      </div>
+      <div class="card-footer-item">
+        <span> Share on <a href="#">Facebook</a> </span>
+      </div>
+    </footer>
+  </div>
+</template>
