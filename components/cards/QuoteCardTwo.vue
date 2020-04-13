b4a0a308dcd2dfcee9c510c99a9342d6395f2204
anneevelyn <62508929+anneevelyn@users.noreply.github.com>
2020-04-12 17:17:29 -0700

    removed share quote footer on quote cards

--- components/cards/QuoteCardTwo.vue
+++ components/cards/QuoteCardTwo.vue
@@ -8,13 +8,5 @@
         ~ Pauline Oliveros
       </p>
     </div>
-    <footer class="card-footer">
-      <div class="card-footer-item">
-        <span> Share on <a href="#">Twitter</a> </span>
-      </div>
-      <div class="card-footer-item">
-        <span> Share on <a href="#">Facebook</a> </span>
-      </div>
-    </footer>
   </div>
 </template>
